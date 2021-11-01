6ab229f39755b4f2f43a23c641ea65ee2580a390
brian99na <brian99na@gmail.com>
2021-11-01 01:18:55 -0700

    Revert "check"
    This reverts commit 151749d65c094091c100a493dae5fde272f4ef34, reversing
    changes made to e77dbbc98ed020455c7332cece91cd883294fc22.

--- crypto-project/src/App.js
+++ crypto-project/src/App.js
@@ -8,7 +8,9 @@ function App() {
   console.log(`$1000 in 2016 is worth $${current} in 2021`)
 
   useEffect(() => {
-    fetch('https://api.polygon.io/v3/reference/tickers?ticker=spy&date=2021-02-25&active=true&sort=ticker&order=asc&limit=10&apiKey=U9EKBFMVPhXgdW2I4JEhsqnqarn3rpcb')
+    fetch('https://api.bls.gov/publicAPI/v1/timeseries/data',{
+      method: 'POST'
+    })
     .then(response => response.json())
     .then(data => console.log(data),console.log('hello'))
   }, [])
