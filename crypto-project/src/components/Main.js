f8a2f3d9b438d851c8ca71d496d66a46cee79aec
brian99na <brian99na@gmail.com>
2021-11-02 14:27:19 -0700

    made API call

--- crypto-project/src/components/Main.js
+++ crypto-project/src/components/Main.js
@@ -1,18 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router';
 import Homepage from './Homepage';
-import About from './About';
-import Contact from './Contact';
 import SecondInvestmentDetails from './SecondInvestmentDetails';
+import PricesDetails from './PricesDetails';
 
 function Main() {
 
     const [price, setPrice] = useState('1000')
     const [date, setDate] = useState('30-03-2017')
     const [coin, setCoin] = useState('bitcoin')
+    const [currentCoinPrice, setCurrentCoinPrice] = useState('')
+    const [pastCoinPrice, setPastCoinPrice] = useState('')
+
+    const currentApiCall = () => {
+        fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd`)
+        .then(res => res.json())
+        .then(data => setCurrentCoinPrice(data.coin.usd))
+    }
+
+    useEffect(() => {
+        currentApiCall()
+    }, [price])
+
+  
+
     const [month, setMonth] = useState('')
     const [year, setYear] = useState('')
-  
     useEffect(() => {
         const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
@@ -25,9 +38,7 @@ function Main() {
     return (
         <div>
             <Route path="/" render={() => <Homepage price={price} setPrice={setPrice} date={date} setDate={setDate} coin={coin} setCoin={setCoin} month={month} year={year}/>}/>
-            <Route path="/About" render={() => <About />}/>
-            <Route path="/Contact" render={() => <Contact />}/>
-            <Route path="/PricesDetails" render={() => <Contact />}/>
+            <Route path="/PricesDetails" render={() => <PricesDetails  />}/>
             <Route path="/SecondInvestmentDetails" render={() => <SecondInvestmentDetails />}/>
         </div>
     )
--- crypto-project/src/components/Prices.js
+++ crypto-project/src/components/Prices.js
@@ -1,15 +1,12 @@
-import React, { useRef } from 'react';
-
-
 function Prices(props) {
-
     
 
     return (
         <div className='prices'>
             <h1 ref={props.pageRef}>Your Investment would now be worth:</h1>
+            <h1></h1>
         </div>
     )
 }
 
-export default Prices
+export default Prices;
